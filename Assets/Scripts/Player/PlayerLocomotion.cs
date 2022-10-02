be6a6ca0dd6864d9171d2273984f4214f2487fae
stynan27 <seamustynan16@gmail.com>
2022-10-01 20:06:36 -0400

    New player model & animations

--- Assets/Scripts/Player/PlayerLocomotion.cs
+++ Assets/Scripts/Player/PlayerLocomotion.cs
@@ -51,6 +51,8 @@ namespace MOOSE
       Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
       rigidbody.velocity = projectedVelocity;
 
+      animatorHandler.UpdateAnimatorValues(inputHandler.moveAmount, 0);
+
       if (animatorHandler.canRotate)
       {
         HandleRotation(delta);
